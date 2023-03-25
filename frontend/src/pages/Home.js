f2a8e490724cca3876b96aec5c405b3621600b7a
ControlettiRockt <128934132+ControlettiRockt@users.noreply.github.com>
2023-03-25 13:58:43 -0700

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>NFT Discovery Platform</h1>
+      <p>Welcome to the NFT Discovery Platform! Explore a curated collection of NFTs from various artists and creators.</p>
+    </div>
+  );
+};
+
+export default Home;
